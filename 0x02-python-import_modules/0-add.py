4f2f8617c59840c037aaf759332ac4d30dc87314
maxwell150 <maxwelgithumbi@gmail.com>
2021-09-09 13:16:23 +0000

    import add func from a .py file

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -2,10 +2,10 @@
 # 0_add.py
 
 
-if __name__ = "__main__":
-	"""sum of 1 and 2"""
-	from add_0 import add
+if __name__ == "__main__":
+    """Print sum of 1 and 2."""
+    from add_0 import add
 
-	a = 1
-	b = 2
-	print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{} + {} = {}".format(a, b, add(a, b)))
