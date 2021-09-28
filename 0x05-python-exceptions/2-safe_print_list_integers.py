7ece8c57c2ca0505d1280d991f3716ffb7e23479
maxwell150 <maxwelgithumbi@gmail.com>
2021-09-28 14:31:50 +0000

    function that prints the 1st  x element of a lst

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# 2-safe_print_list_integers.py
+
+
+def safe_print_list_integers(my_list=[], x=0):
+    """prints the first x elements of a lst and only int"""
+    count = 0
+    for i in range(0, x):
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            count += 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return (count)
