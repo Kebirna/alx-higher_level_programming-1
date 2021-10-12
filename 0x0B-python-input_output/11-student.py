ed612de2bb2c144dc7282746d9ceaa49d1c22785
maxwell150 <maxwelgithumbi@gmail.com>
2021-10-12 18:50:06 +0000

    input_output

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,48 +1,27 @@
-#!/usr/bin/pyton3
-"""Defiantion for a student class
-"""
+#!/usr/bin/python3
+"""json"""
 
 
 class Student:
-   """a class for a student
-   """
+    """A student."""
 
-   def __init__(self, first_name, last_name, age):
-       """Initalizes a student object
-       """
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-       self.first_name = first_name
-       self.last_name = last_name
-       self.age = age
+    def to_json(self, attrs=None):
+        """Retrieve a dictionary representation of a Student instance."""
+        if attrs is not None and all(isinstance(x, str) for x in attrs):
+            d = {}
+            for k, v in self.__dict__.items():
+                if k in attrs:
+                    d[k] = v
+            return d
+        else:
+            return self.__dict__
 
-   def to_json(self, attrs=None):
-       """returns a json representatiion of
-       the object
-       """
-
-       if attrs != None and not isinstance(attrs, list):
-           raise TypeError("attrs must me a list of strings")
-
-       if attrs != None and any(not isinstance(attr, str) for attr in attrs):
-           raise TypeError("attrs must me a list of strings")
-
-       if attrs == None:
-           return self.__dict__
-
-       json_dic = {}
-       for key in attrs:
-           if key in self.__dict__.keys():
-               json_dic[key] = self.__dict__[key]
-
-       return json_dic
-
-    def relod_from_json(self, json):
-    """replaces object's attributes
-    """
-
-        for k, v in json.items():
-            setattr(self, k, v)
-
-
-
-
+    def reload_from_json(self, json):
+        """reload data from json"""
+        for (key, value) in json.items():
+            setattr(self, key, value)
