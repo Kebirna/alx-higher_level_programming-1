09e3b688f3b7fa11abeb166c36815cc1e24bcd1e
maxwell150 <maxwelgithumbi@gmail.com>
2021-10-12 18:43:44 +0000

    input_output

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,24 +1,14 @@
 #!/usr/bin/python3
-"""Defiens a student class
-that have to_json public method"""
+"""class student"""
+
 
 class Student:
-   """a class for a student
-   """
+    """student"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-   def __init__(self, first_name, last_name, age):
-       """Initalizes a student object
-       """
-
-       self.first_name = first_name
-       self.last_name = last_name
-       self.age = age
-
-   def to_json(self):
-       """returns a json representatiion of
-       the object
-       """
-
-       return self.__dict__
-
-
+    def to_json(self):
+        """to_json"""
+        return self.__dict__
