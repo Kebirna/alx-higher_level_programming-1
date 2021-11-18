840f8b345e1ea7b40ee4ccd32c230f4f1ae8d75c
maxwell150 <maxwelgithumbi@gmail.com>
2021-11-18 22:46:13 +0300

    more-queries

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- script that creates the mysql server user
-CREATE user IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
+CREATE USER IF NOT EXISTS `user_0d_1`@`localhost` IDENTIFIED BY `user_0d_1_pwd`;
 GRANT ALL PRIVILEGES ON *.* TO `user_0d_01`@`localhost`;
+FLUSH PRIVILEGES;
