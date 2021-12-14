ec786a7a47b86a75b3c67912cfedc496b05441a6
maxwell150 <maxwelgithumbi@gmail.com>
2021-12-14 16:34:21 +0300

    fix

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const times = Number(process.argv[2]);
+const times = Math.floor(Number(process.argv[2]));
 if (isNaN(times)) {
   console.log('Missing number of occurrences');
 } else {
