6f7bd7710f73a6e9dc2e4644d7ef60c8ce7c3eb2
max <maxwelgithumbi@gmail.com>
2021-12-18 17:18:57 +0000

    lst states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,7 +6,7 @@ import MySQLdb
 def list_states():
     """lists all states from the database hbtn_0e_0_usa"""
 
-    conn = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    conn = MySQLdb.connect(host='localhost', port=3306, user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     cur = conn.cursor()
     cur.execute('SELECT * FROM `states`')
     [print(state) for state in cur.fetchall()]
