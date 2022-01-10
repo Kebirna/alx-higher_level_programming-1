2218e7f5e6ef27409498bcf109b6a953773f06e8
max <maxwelgithumbi@gmail.com>
2022-01-10 12:11:58 +0300

    py script

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """
  script that fetches https://intranet.hbtn.io/status
 """
@@ -11,5 +10,5 @@ if __name__ == '__main__':
     req = requests.get(url)
     print('Body response:')
     print('\t- type: {}'.format(type(req.text)))
-    print('\t- content: {}'.format(type(req.text)))
+    print('\t- content: {}'.format(req.text))
 
