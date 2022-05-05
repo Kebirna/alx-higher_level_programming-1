3fc323e8c40db0fe922c1d352e2efed6693e1b9e
max <maxwelgithumbi@gmail.com>
2022-05-05 16:41:14 +0300

    num of shows by genre

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,6 @@
 -- lists all genres from hbnt_0d_shows
-SELECT tv_genres.name, COUNT(*) AS num
+SELECT tv_genres.name AS genre, COUNT(*) AS number_of_shows
 FROM tv_genres
-GROUP BY tv_shows.name
-INNER JOIN tv_show_genres
-ON tv_shows.id = tv_show_genres.show_id
-ORDER BY num DESC;
+INNER JOIN tv_show_genres ON tv_show_genres.genre_id=tv_genres.id
+GROUP BY genre
+ORDER BY number_of_shows DESC;
