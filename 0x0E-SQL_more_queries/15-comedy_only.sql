89d673ddc901beef222da319de84d08f5eb3ebe6
max <maxwelgithumbi@gmail.com>
2022-05-05 16:44:43 +0300

    comedy

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -1,6 +1,7 @@
 -- list comdy shows
 SELECT tv_shows.title
 FROM tv_shows
-LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres_id
-INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-WHERE tv_genres.name = 'Comedy';
+JOIN tv_show_genres ON tv_shows.id=tv_show_genres.show_id
+JOIN tv_genres ON tv_genres.id=tv_show_genres.genre_id
+WHERE tv_genres.name="Comedy"
+ORDER BY tv_shows.title ASC;
