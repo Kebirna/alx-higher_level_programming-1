dd59e7eb37cab41981acb23e1b142fa867a9edfd
max <maxwelgithumbi@gmail.com>
2022-05-05 16:28:00 +0300

    states table

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,7 +1,3 @@
 -- script that creates the db hbtn_0d_usa and table states
-CREATE database IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS `hbtn_0d_usa.states`(
-	id INT UNIQUE AUTO_INCREMENT NOT NULL,
-	name varchar(256) NOT NULL,
-	PRIMARY KEY(`id`)
-);
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states(id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY, name VARCHAR(256) NOT NULL);
